345eb3c20be346f71967ec16841dd281a5df4f5d
민수 정 <utopia0716@gmail.com>
2020-01-09 22:46:01 +0900

    초기 셋팅

---
+++ BaekJoon/BaekJoon/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BaekJoon
+//
+//  Created by 민수 정  on 09/01/2020.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

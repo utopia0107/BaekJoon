069d1f840e80f6c2dd707a3ccfba2e18bf436098
utopia0107 <utopia0716@gmail.com>
2020-05-21 15:16:50 +0900

    1012번 - 유기농 배추

---
+++ BaekJoon/BaekJoon/1012.h
@@ -0,0 +1,13 @@
+//
+//  1012.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/05/21.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _012_h
+#define _012_h
+
+
+#endif /* _012_h */
--- BaekJoon/BaekJoon/main.cpp
+++ BaekJoon/BaekJoon/main.cpp
@@ -8,7 +8,7 @@
 
 #include <iostream>
 
-#include "10866.h"
+#include "1012.h"
 int main() {
     solution();
     return 0;

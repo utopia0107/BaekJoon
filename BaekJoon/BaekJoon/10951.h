7e0954d8ec0dae2296f91fd368dc3b9a84e1f69f
utopia0107 <utopia0716@gmail.com>
2020-05-21 14:39:20 +0900

    10951번 - A+B - 4

---
+++ BaekJoon/BaekJoon/10951.h
@@ -0,0 +1,13 @@
+//
+//  10951.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/05/21.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _0951_h
+#define _0951_h
+
+
+#endif /* _0951_h */

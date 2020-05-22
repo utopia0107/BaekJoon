b87a33752f8ca1ad1b78e7af172f9ebba0c6ef29
utopia0107 <utopia0716@gmail.com>
2020-05-22 17:49:33 +0900

    7662번 - 이중 우선순위 큐

---
+++ BaekJoon/BaekJoon/7662.h
@@ -0,0 +1,13 @@
+//
+//  7662.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/05/21.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _662_h
+#define _662_h
+
+
+#endif /* _662_h */
--- BaekJoon/BaekJoon/main.cpp
+++ BaekJoon/BaekJoon/main.cpp
@@ -8,7 +8,7 @@
 
 #include <iostream>
 
-#include "1012.h"
+#include "7662.h"
 int main() {
     solution();
     return 0;

b176d5b03b7f30e9f2391ebddc790115e4372a9e
utopia0107 <utopia0716@gmail.com>
2020-05-28 16:23:55 +0900

    1922번 - 네트워크 연결

---
+++ BaekJoon/BaekJoon/1922.h
@@ -0,0 +1,13 @@
+//
+//  1922.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/05/28.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _922_h
+#define _922_h
+
+
+#endif /* _922_h */
--- BaekJoon/BaekJoon/main.cpp
+++ BaekJoon/BaekJoon/main.cpp
@@ -8,7 +8,7 @@
 
 #include <iostream>
 
-#include "7662.h"
+#include "1922.h"
 int main() {
     solution();
     return 0;

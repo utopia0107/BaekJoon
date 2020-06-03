7d0ca88d410d08f467c4c9ca8664fefba3a81a03
utopia0107 <utopia0716@gmail.com>
2020-06-03 19:53:41 +0900

    2252번 - 줄 세우기

---
+++ BaekJoon/BaekJoon/2252.h
@@ -0,0 +1,13 @@
+//
+//  2252.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/03.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _252_h
+#define _252_h
+
+
+#endif /* _252_h */

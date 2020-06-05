2e6c138842eeeaca0c51beefae401cdbd58c30ab
utopia0107 <utopia0716@gmail.com>
2020-06-05 13:15:53 +0900

    1005번 - ACM Craft

---
+++ BaekJoon/BaekJoon/1005.h
@@ -0,0 +1,13 @@
+//
+//  1005.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/05.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _005_h
+#define _005_h
+
+
+#endif /* _005_h */

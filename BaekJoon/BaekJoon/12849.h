972f9ebe42a61c12224da0f358b74f7aac4ffce9
utopia0107 <utopia0716@gmail.com>
2020-06-12 23:04:45 +0900

    12849번 - 본대 산책

---
+++ BaekJoon/BaekJoon/12849.h
@@ -0,0 +1,13 @@
+//
+//  12849.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/12.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _2849_h
+#define _2849_h
+
+
+#endif /* _2849_h */

62d726fd84355cb4da0003c1e0f5ba1443b15286
utopia0107 <utopia0716@gmail.com>
2020-06-16 15:51:21 +0900

    1043번 - 거짓말

---
+++ BaekJoon/BaekJoon/1043.h
@@ -0,0 +1,13 @@
+//
+//  1043.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/16.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _043_h
+#define _043_h
+
+
+#endif /* _043_h */

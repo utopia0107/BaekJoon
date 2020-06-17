2f8a21e7c9c58ff8eba7cc2a73aa3e18472989ce
utopia0107 <utopia0716@gmail.com>
2020-06-17 20:26:43 +0900

    6118번 - 숨바꼭질

---
+++ BaekJoon/BaekJoon/6118.h
@@ -0,0 +1,13 @@
+//
+//  6118.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/17.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _118_h
+#define _118_h
+
+
+#endif /* _118_h */

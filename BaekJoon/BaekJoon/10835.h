defddc9e234d678dd3c5c29013ab22756496e1f4
utopia0107 <utopia0716@gmail.com>
2020-06-18 14:41:25 +0900

    10835번 - 카드게임

---
+++ BaekJoon/BaekJoon/10835.h
@@ -0,0 +1,13 @@
+//
+//  10835.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/18.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _0835_h
+#define _0835_h
+
+
+#endif /* _0835_h */

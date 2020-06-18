0cf25dbb6940ff1929bcf9aee56f652047af30ea
utopia0107 <utopia0716@gmail.com>
2020-06-18 12:12:41 +0900

    6593번 - 상범 빌딩

---
+++ BaekJoon/BaekJoon/6593.h
@@ -0,0 +1,13 @@
+//
+//  6593.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/17.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _593_h
+#define _593_h
+
+
+#endif /* _593_h */

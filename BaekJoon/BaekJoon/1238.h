8c5c00533be00484b595bf9618758f0e27843ff5
utopia0107 <utopia0716@gmail.com>
2020-06-18 12:34:14 +0900

    1238번 - 파티

---
+++ BaekJoon/BaekJoon/1238.h
@@ -0,0 +1,13 @@
+//
+//  1238.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/18.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _238_h
+#define _238_h
+
+
+#endif /* _238_h */

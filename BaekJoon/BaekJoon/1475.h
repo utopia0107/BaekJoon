8be906f1c61ddac3c2ae69823d25ad5289ad7c44
utopia0107 <utopia0716@gmail.com>
2020-06-19 22:52:40 +0900

    1475번 -  방 번호

---
+++ BaekJoon/BaekJoon/1475.h
@@ -0,0 +1,13 @@
+//
+//  1475.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/19.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _475_h
+#define _475_h
+
+
+#endif /* _475_h */

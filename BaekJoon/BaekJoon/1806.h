f10c0b39716ac5ca3030f30b0ea7cc4aae888d27
utopia0107 <utopia0716@gmail.com>
2020-06-24 14:02:33 +0900

    1806번 - 부분합

---
+++ BaekJoon/BaekJoon/1806.h
@@ -0,0 +1,13 @@
+//
+//  1806.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/24.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _806_h
+#define _806_h
+
+
+#endif /* _806_h */

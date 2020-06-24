0670d0b70c1110c094fbc698468cd0e8a5c06a49
utopia0107 <utopia0716@gmail.com>
2020-06-24 13:13:44 +0900

    1987번 - 알파벳

---
+++ BaekJoon/BaekJoon/1987.h
@@ -0,0 +1,13 @@
+//
+//  1987.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/24.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _987_h
+#define _987_h
+
+
+#endif /* _987_h */

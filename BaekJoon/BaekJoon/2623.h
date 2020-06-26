678407db12955a618ba57bf0a4c66becdb1a0cb9
utopia0107 <utopia0716@gmail.com>
2020-06-26 12:47:52 +0900

    2623번 - 음악프로그램

---
+++ BaekJoon/BaekJoon/2623.h
@@ -0,0 +1,13 @@
+//
+//  2623.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/26.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _623_h
+#define _623_h
+
+
+#endif /* _623_h */

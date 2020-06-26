a7cd0f818f95fef956458e59a41eafcbb654c5a9
utopia0107 <utopia0716@gmail.com>
2020-06-26 14:06:46 +0900

    2887번 - 행성 터널

---
+++ BaekJoon/BaekJoon/2887.h
@@ -0,0 +1,13 @@
+//
+//  2887.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/26.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _887_h
+#define _887_h
+
+
+#endif /* _887_h */

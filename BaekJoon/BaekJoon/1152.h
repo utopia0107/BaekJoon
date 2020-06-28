7c94951ec735680c9f2680b20ac6a5207186b5d0
utopia0107 <utopia0716@gmail.com>
2020-06-28 18:25:28 +0900

    1152번 - 단어의 개수

---
+++ BaekJoon/BaekJoon/1152.h
@@ -0,0 +1,13 @@
+//
+//  1152.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/28.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _152_h
+#define _152_h
+
+
+#endif /* _152_h */

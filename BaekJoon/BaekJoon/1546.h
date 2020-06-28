e047d402d8b24c4c26bb67e314e33992becb8b3e
utopia0107 <utopia0716@gmail.com>
2020-06-28 18:42:08 +0900

    1546번 - 평균

---
+++ BaekJoon/BaekJoon/1546.h
@@ -0,0 +1,13 @@
+//
+//  1546.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/28.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _546_h
+#define _546_h
+
+
+#endif /* _546_h */

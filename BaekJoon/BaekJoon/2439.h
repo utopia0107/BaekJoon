1b29fb1577a31e3731e2e8dd0ff466fdcc60f4e3
utopia0107 <utopia0716@gmail.com>
2020-06-28 18:45:12 +0900

    2439번 - 별 찍기 - 2

---
+++ BaekJoon/BaekJoon/2439.h
@@ -0,0 +1,13 @@
+//
+//  2439.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/28.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _439_h
+#define _439_h
+
+
+#endif /* _439_h */

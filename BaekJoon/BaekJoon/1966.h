e942ebf90451d6f90c9462647a57b489353e1cf9
utopia0107 <utopia0716@gmail.com>
2020-06-29 13:16:04 +0900

    1966번 - 프린터 큐

---
+++ BaekJoon/BaekJoon/1966.h
@@ -0,0 +1,13 @@
+//
+//  1966.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/29.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _966_h
+#define _966_h
+
+
+#endif /* _966_h */

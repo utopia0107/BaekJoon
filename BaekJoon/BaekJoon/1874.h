0977af9c51b893da1871215d074cec7cf10e29b2
utopia0107 <utopia0716@gmail.com>
2020-06-29 12:41:53 +0900

    1874번 - 스택 수열

---
+++ BaekJoon/BaekJoon/1874.h
@@ -0,0 +1,13 @@
+//
+//  1874.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/29.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _874_h
+#define _874_h
+
+
+#endif /* _874_h */

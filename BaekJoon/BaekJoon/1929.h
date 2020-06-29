c466f54f9b40996c3d68c5921824313513b5357d
utopia0107 <utopia0716@gmail.com>
2020-06-29 12:47:46 +0900

    1929번 -  소수 구하기

---
+++ BaekJoon/BaekJoon/1929.h
@@ -0,0 +1,13 @@
+//
+//  1929.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/06/29.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _929_h
+#define _929_h
+
+
+#endif /* _929_h */

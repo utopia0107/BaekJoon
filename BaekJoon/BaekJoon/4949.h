80dcd08f91587819fee57221ab6059782559777c
utopia0107 <utopia0716@gmail.com>
2020-07-01 11:50:03 +0900

    4949번 -  균형잡힌 세상

---
+++ BaekJoon/BaekJoon/4949.h
@@ -0,0 +1,13 @@
+//
+//  4949.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/07/01.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _949_h
+#define _949_h
+
+
+#endif /* _949_h */

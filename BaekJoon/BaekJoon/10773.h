b9caa4bac49cc9e4b014b33e83105d34f4e94e15
utopia0107 <utopia0716@gmail.com>
2020-07-03 21:10:55 +0900

    10773번 - 제로

---
+++ BaekJoon/BaekJoon/10773.h
@@ -0,0 +1,13 @@
+//
+//  10773.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/07/03.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _0773_h
+#define _0773_h
+
+
+#endif /* _0773_h */

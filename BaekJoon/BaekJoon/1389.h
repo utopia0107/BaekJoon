c95406ffe90729dd6cf2a650e5cb6d2fa6c567cd
utopia0107 <utopia0716@gmail.com>
2020-07-13 12:50:07 +0900

    1389번 - 케빈 베이컨의 6단계 법칙

---
+++ BaekJoon/BaekJoon/1389.h
@@ -0,0 +1,13 @@
+//
+//  1389.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/07/13.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _389_h
+#define _389_h
+
+
+#endif /* _389_h */

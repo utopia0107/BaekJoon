feacc18fbb952ec6b835c4c36584ba874b59d6c8
utopia0107 <utopia0716@gmail.com>
2020-07-13 12:15:05 +0900

    1780번 - 종이의 개수

---
+++ BaekJoon/BaekJoon/1780.h
@@ -0,0 +1,13 @@
+//
+//  1780.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/07/13.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _780_h
+#define _780_h
+
+
+#endif /* _780_h */

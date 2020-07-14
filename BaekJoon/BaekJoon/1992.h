ae15ce8699b01048ef8708f0a800a3993c0a4cff
utopia0107 <utopia0716@gmail.com>
2020-07-14 14:51:28 +0900

    1992번 - 쿼드트리

---
+++ BaekJoon/BaekJoon/1992.h
@@ -0,0 +1,13 @@
+//
+//  1992.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/07/14.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _992_h
+#define _992_h
+
+
+#endif /* _992_h */

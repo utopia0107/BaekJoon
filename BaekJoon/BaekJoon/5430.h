f6a8a37d3a69fd37c94d0fb366b92708e5542b48
utopia0107 <utopia0716@gmail.com>
2020-07-14 14:03:11 +0900

    5430번 - AC

---
+++ BaekJoon/BaekJoon/5430.h
@@ -0,0 +1,13 @@
+//
+//  5430.h
+//  BaekJoon
+//
+//  Created by 정민수 on 2020/07/14.
+//  Copyright © 2020 Minsu Jeong. All rights reserved.
+//
+
+#ifndef _430_h
+#define _430_h
+
+
+#endif /* _430_h */
